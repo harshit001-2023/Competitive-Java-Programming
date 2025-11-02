c49457d9719169ab5af481e0e5c77bd625a756df
harshit001-2023 <harshitpande938@gmail.com>
2025-11-02 18:25:19 +0530

    Solved a new problem for merging two Linked lists [02/11/2025]

--- MergeLists.java
+++ MergeLists.java
@@ -25,7 +25,27 @@ public class MergeLists {
     }
 
     public static void main(String[] args) {
+        // 1. Manually create the first list: [1, 2, 4]
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
 
+        // 2. Manually create the second list: [1, 3, 4]
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        // 3. Create an object and call your method
+        MergeLists solver = new MergeLists();
+        ListNode mergedList = solver.mergeTwoLists(l1, l2);
+
+        // 4. Print the result
+        System.out.println("Merged list:");
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " -> ");
+            mergedList = mergedList.next;
+        }
+        System.out.println("null");
     }
 }
 
