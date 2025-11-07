40d67e3cb900e724e3d1c8e9cad2be1ecece2908
harshit001-2023 <harshitpande938@gmail.com>
2025-11-07 17:17:18 +0530

    Solving a problem [07/11/2025]

--- PrimeNumberUptoN.java
+++ PrimeNumberUptoN.java
@@ -5,7 +5,24 @@ public class PrimeNumberUptoN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number");
-        int num = ;
+        int num = sc.nextInt();
+
+        if (num <= 1){
+            System.out.println("Invalid");
+        }else{
+            System.out.println("Prime no from 1 - "+num);
+            for(int i = 2; i <= num; i++){
+                int isPrime = 1;
+                for (int j = 2; j * j <= i; j++){
+                    if (i%j==0) {
+                        isPrime = 0;
+                        break;
+                    }
+                }
+                if (isPrime == 1) System.out.print(i+" ");
+
+            }
+        }
 
 
     }
