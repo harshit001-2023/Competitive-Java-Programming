821be8c17e3966cb1b6fbaa3dfd3594e5f6735e2
harshit001-2023 <harshitpande938@gmail.com>
2025-11-10 13:47:22 +0530

    Sovled one pattern today [10/11/2025]

--- Patterns.java
+++ Patterns.java
@@ -233,8 +233,9 @@ public class Patterns {
 		int c = 1;
 		for (int i = 0; i <= n; i++){
 			for (int j = 0; j <= n; j++){
-				if(i%2==0) System.out.print(1+" ");
+				if(c%2!=0) System.out.print(1+" ");
 				else System.out.print(0+" ");
+//				System.out.print(c+" ");
 				c++;
 			}
 			System.out.println();
