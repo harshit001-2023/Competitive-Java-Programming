ee92b992ee999cb2dee70abf792336bbac6d8677
harshit001-2023 <harshitpande938@gmail.com>
2025-11-18 21:53:58 +0530

    Solved some basic problems [18/11/2025]

---
+++ Basics/CalTax.java
@@ -0,0 +1,10 @@
+void main(String []x){
+	//int a = Integer.parseInt(x[]);
+	double tax_rate = 4.9/100;
+	int income = 990000;
+	
+	double tax = income * tax_rate;
+	
+	IO.println("Tax : "+tax);
+	
+}
---
+++ Basics/CubeCmd.java
@@ -0,0 +1,5 @@
+void main(String []x){
+	int a = Integer.parseInt(x[0]), cube = 1;
+	cube = a * a * a;
+	IO.println("Cube of "+a+" is "+cube);
+}
