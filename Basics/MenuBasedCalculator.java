35730c37d138c1122aa7fa7d7dad1663c6189410
harshit001-2023 <harshitpande938@gmail.com>
2025-11-26 22:04:29 +0530

    Solved a problem Menu Based Calculator [26/11/2025]

---
+++ Basics/MenuBasedCalculator.java
@@ -0,0 +1,34 @@
+/*
+Enter first number : 12
+Enter second number : 14
+Choose operation: ADD / SUB / MUL / DIV
+Output:
+add
+Result: 26
+Important Note
+Your code must be able to print the sample output from the provided sample input.
+However, your solution is also run against multiple hidden test cases.
+You must pass these hidden test cases to fully solve the problem.
+*/
+
+
+void main(){
+    int x = Integer.parseInt(IO.readln());
+    int y = Integer.parseInt(IO.readln());
+
+    String operation = IO.readln().toUpperCase();
+
+    String result = switch(operation){
+        case "ADD" -> "Result: "+(x+y);
+        case "SUB" -> "Result: "+(x-y);
+        case "MUL" -> "Result: "+(x*y);
+        case "DIV" -> {
+            if(y==0) yield "Not divided by zero";
+            else yield "Result: "+(x/y);
+        }
+        default -> "Invalid Operation.";
+    };
+
+    IO.println(result);
+
+}
