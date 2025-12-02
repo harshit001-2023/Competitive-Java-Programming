0df34b09aee6b297bea285dce029c9e74e810c48
harshit001-2023 <harshitpande938@gmail.com>
2025-12-02 17:05:50 +0530

    Updated and formated some yesterday's program [02/12/2025]

--- Basics/blc/TwoDigitsSum.java
+++ Basics/blc/TwoDigitsSum.java
@@ -1,6 +1,7 @@
 package Basics.blc;
 
 public class TwoDigitsSum{
+    // Simple Logic for calculating sum
     public static int getSumOfDigits(int num1, int num2){
         return (num1+num2);
     }
--- Basics/elc/SumOfTwoDigits.java
+++ Basics/elc/SumOfTwoDigits.java
@@ -4,10 +4,15 @@ import Basics.blc.TwoDigitsSum;
 
 public class SumOfTwoDigits {
     void main(){
+        // Taking user input
         IO.println("Enter two numbers : ");
         int num1 = Integer.parseInt(IO.readln());
         int num2 = Integer.parseInt(IO.readln());
+
+        // Assigning answer of getSumOfDeigits() method of class SumOfTwoDigits to sum variable
         int sum = TwoDigitsSum.getSumOfDigits(num1, num2);
+
+        // Calling and printing sum from SumOfTwoDigits class method
         IO.println(sum);
     }
 }
