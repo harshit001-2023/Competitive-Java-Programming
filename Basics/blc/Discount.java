07e07cc0d4e82fe6260ba4feb2551da622ec33fa
harshit001-2023 <harshitpande938@gmail.com>
2025-12-02 16:45:26 +0530

    Updated and formated some yesterday's program [01/12/2025]

--- Basics/blc/Discount.java
+++ Basics/blc/Discount.java
@@ -1,10 +1,16 @@
 package Basics.blc;
 
 public class Discount {
+
+    // Creating static method for discount calculation which returns double format
     public static double calculateDiscount(double amount){
+        // If Amount is more than 5k then give 20% of discount
         if(amount >= 5000) return amount-(amount * 0.20);
+            // If Amount is in between 5k and 3k then give 15% of discount
         else if (amount < 5000 && amount >= 3000) return amount - (amount * 0.15);
+            // If Amount is in between 3k and 1k then give 10% of discount
         else if (amount < 3000 && amount >= 1000) return amount - (amount * 0.10);
+            // If Amount is less than 1k then give 5% of discount
         else return amount - (amount*0.05);
         }
 }
--- Basics/elc/ShoppingCart.java
+++ Basics/elc/ShoppingCart.java
@@ -7,12 +7,14 @@ import java.util.Scanner;
 
 public class ShoppingCart {
     void main(){
+        // Taking input from user
         Scanner sc = new Scanner(System.in);
         IO.println("Enter you name: ");
         String name = sc.next();
         IO.println("Enter your bill Amount : ");
         double amount = sc.nextDouble();
 
+        // Calling and Printing methods from Discount BLC class
         IO.println("Customer name: "+name.toUpperCase(Locale.ROOT));
         IO.println("Original Bill: "+amount);
         IO.println("Amount Payable after Discount "+ Discount.calculateDiscount(amount));
