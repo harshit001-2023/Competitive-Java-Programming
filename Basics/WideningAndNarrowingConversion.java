93c60281c4a48b6247972f6997100f598ff6a66b
harshit001-2023 <harshitpande938@gmail.com>
2025-12-06 22:05:10 +0530

    Added Programs from todays java test [06/12/2025]

---
+++ Basics/WideningAndNarrowingConversion.java
@@ -0,0 +1,44 @@
+/*Problem Description
+3) Write a Java program that demonstrates method overloading using:
+
+Widening conversion – passing a smaller data type to a method that accepts a larger data type.
+(Example: int → double)
+
+Narrowing conversion – converting a larger data type into a smaller data type inside the method.
+(Example: double → int)
+
+Examples
+Example 1
+Input:
+10
+4.67
+Output:
+Widening Conversion Method Called
+Received double value: 10.0
+
+Narrowing Conversion Method Called
+Original double value: 45.67
+Narrowed int value: 45*/
+
+void main(){
+    int num = Integer.parseInt(IO.readln());
+    double num1 = Double.parseDouble(IO.readln());
+
+    wideningConversion(num);
+    IO.println();
+    narrowingConversion(num1);
+
+}
+
+void wideningConversion(int num){
+    IO.println("Widening Conversion Method Called");
+    Double n = (double)num;
+    IO.println("Received double value: "+n);
+}
+
+void narrowingConversion(double num1){
+    IO.println("Narrowing Conversion Method Called");
+    IO.println("Original double value: "+num1);
+    Integer n = (int)num1;
+    IO.println("Narrowed int value: "+n);
+}
