7dda9d62cc735c16385b07dc93d01b17308f76ef
harshit001-2023 <harshitpande938@gmail.com>
2025-12-15 22:10:52 +0530

    Updated and added some Java problems solutions [15/12/2025]

---
+++ MissingChar.java
@@ -0,0 +1,23 @@
+public class MissingChar {
+    static void main() {
+        // Input and Printing Result
+        String str = IO.readln("Enter a word: ");
+        int n = Integer.parseInt(IO.readln("Enter a num: "));
+        IO.println(missingChar(str, n));
+    }
+
+
+    public static String missingChar(String str, int n) {
+        // Edge case if Num is more than String size then error
+        if(n >= str.length()) {
+            System.err.println("Enter valid number according to String size.");
+            System.exit(0);
+        }
+        // Calculating front
+        // Ex. str = Mission, n = 2
+        String front = str.substring(0,n); // 0,n = 0,2 = front : Mi
+        String back = str.substring((n+1),str.length()); // 2+1, 7 = 3,7= back : sion
+        return front + back; // Mi + sion = Mision
+    }
+
+}
