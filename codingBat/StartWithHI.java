1350d18332c88b46442e231c7effcde0fda72ab1
harshit001-2023 <harshitpande938@gmail.com>
2025-12-18 23:16:57 +0530

    Updated and added some Java problems solutions [18/12/2025]

--- codingBat/StartWithHI.java
+++ codingBat/StartWithHI.java
@@ -15,7 +15,7 @@ public class StartWithHI {
     }
 
     public static boolean startHi(String str) {
-        if(str.substring(0,2).equalsIgnoreCase("hi")) return true;
-        else return false;
+        if(str.length() < 2) return false;
+        else return (str.substring(0,2).equalsIgnoreCase("hi"));
     }
 }
