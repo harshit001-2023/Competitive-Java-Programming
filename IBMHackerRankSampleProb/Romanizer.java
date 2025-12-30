b6f0cc51e8537ad429c1090890df1b7ae250056d
harshit001-2023 <harshitpande938@gmail.com>
2025-12-30 22:36:24 +0530

    Updated and added some Java problems solutions [28/12/2025]

---
+++ IBMHackerRankSampleProb/Romanizer.java
@@ -0,0 +1,81 @@
+package IBMHackerRankSampleProb;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result1 {
+
+    /*
+     * Complete the 'romanizer' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts INTEGER_ARRAY numbers as parameter.
+     */
+
+    public static List<String> romanizer(List<Integer> numbers) {
+        // Write your code here
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        List<String> result = new ArrayList<>();
+
+        for(int number : numbers){
+            StringBuilder roman = new StringBuilder();
+            int remaining = number;
+
+            for(int i = 0; i < values.length; i++){
+                while(remaining >= values[i]){
+                    roman.append(symbols[i]);
+                    remaining -= values[i];
+                }
+            }
+            result.add(roman.toString());
+        }
+        return result;
+
+    }
+
+}
+
+public class Romanizer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int numbersCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> numbers = IntStream.range(0, numbersCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<String> result = Result1.romanizer(numbers);
+
+        bufferedWriter.write(
+                result.stream()
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
