12351571d451fccfeadc4a7f67825bb444b2ee86
harshit001-2023 <harshitpande938@gmail.com>
2026-01-16 16:43:39 +0530

    Soled some programs on CodingBat [16/01/2026]

--- StringMatch.java
+++ StringMatch.java
@@ -20,3 +20,15 @@ public int stringMatch(String a, String b) {
 
     return count;
 }
+
+
+public String stringX(String str) {
+    String result = "";
+    for (int i=0; i<str.length(); i++) {
+        // Only append the char if it is not the "x" case
+        if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+            result = result + str.substring(i, i+1); // Could use str.charAt(i) here
+        }
+    }
+    return result;
+}
