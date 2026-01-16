345bd42700ec1033ccdd1ca16da1113cc60a5a88
harshit001-2023 <harshitpande938@gmail.com>
2026-01-16 18:59:42 +0530

    Soled some programs on CodingBat [16/01/2026]

--- StringMatch.java
+++ StringMatch.java
@@ -1,7 +1,8 @@
 void main() {
-    IO.println(stringMatch("xxcaazz", "xxbaaz"));
-    IO.println(stringX("xxHalxxlxoxx"));
-    IO.println(stringX1("x"));
+//    IO.println(stringMatch("xxcaazz", "xxbaaz"));
+//    IO.println(stringX("xxHalxxlxoxx"));
+//    IO.println(stringX1("x"));
+    IO.println(altPairs("abcdefghij"));
 }
 
 /*
@@ -47,4 +48,22 @@ public String stringX1(String str) {
         return ("x".concat(newStr).concat("x"));
     }
     return (str.replaceAll("[x]", ""));
+}
+
+/*Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+altPairs("kitten") → "kien"
+altPairs("Chocolate") → "Chole"
+altPairs("CodingHorror") → "Congrr"*/
+
+public String altPairs(String str){
+    String res = "";
+
+    for(int i = 0; i < str.length(); i+=4){
+        int end = i+2;
+        if(end>str.length()){
+            end = str.length();
+        }
+        res = res + str.substring(i, end);
+    }
+    return res;
 }
