ca069e1d8f2668199b0b807c2a1286c480846923
harshit001-2023 <harshitpande938@gmail.com>
2026-01-18 16:26:03 +0530

    Soled some programs on CodingBat [18/01/2026]

--- WarmUp2.java
+++ WarmUp2.java
@@ -33,7 +33,11 @@ public boolean array123(int[] nums) {
     return false;
 }
 
-/**/
+/*
+Given an array of ints, return the number of times that two 6's are next to each other in the array. Also count instances where the second "6" is actually a 7.
+array667([6, 6, 2]) → 1
+array667([6, 6, 2, 6]) → 1
+array667([6, 7, 2, 6]) → 1*/
 public int array667(int[] nums) {
     int count = 0;
 
