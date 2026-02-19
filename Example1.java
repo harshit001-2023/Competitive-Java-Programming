b66debbd407855687bdb9bbf993b4f27e234e251
harshit001-2023 <harshitpande938@gmail.com>
2026-02-19 23:54:01 +0530

    Working with Java Streams [19/02/2026]

--- Example1.java
+++ Example1.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 void main() {
     int []arr = {10, 80, 6, 5, 20, 44, 66};
+}
 
-    int sum = IntStream.of(arr).sum();
-    IO.println(sum);
+public static int arraySum(int[] arr) {
+    return IntStream.of(arr).sum();
 }
